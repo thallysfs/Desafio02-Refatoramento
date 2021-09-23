8d448815887958cca0905374af52dfeb8538b1cf
thallysfs <thallys@hotmail.com>
2021-09-23 10:31:04 -0300

    refatoramenro Sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+
+interface MovieProps {
+  movies:{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  };
+  genreTitle: string;
+}
+
+export function Content({movies, genreTitle}: MovieProps) {
   // Complete aqui
+  return(
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {genreTitle}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import React, { useEffect, useState } from 'react'
+
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  selectedGenreId: number;
+  onSetSelectedGenreId: (id:number)=> void;
+}
+
+export function SideBar({genres, selectedGenreId, onSetSelectedGenreId}: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSetSelectedGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
